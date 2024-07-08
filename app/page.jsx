8f8b762cd3eec7073455ef2a8e32c11279adb5d6
Nurarat Sangreuang <nurarat0024@gmail.com>
2024-07-08 12:34:40 +0700

    download cv function

--- app/page.jsx
+++ app/page.jsx
@@ -6,6 +6,15 @@ import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
 const Home = () => {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "/assets/cv-nura.pdf"; // Path to the CV file
+    link.download = "cv-nura.pdf"; // Name of the downloaded file
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
@@ -26,6 +35,7 @@ const Home = () => {
                 variant="outline"
                 size="lg"
                 className="uppercase flex items-center gap-2 "
+                onClick={handleDownload}
               >
                 <span>Download CV</span>
                 <FiDownload className="text-xl" />
